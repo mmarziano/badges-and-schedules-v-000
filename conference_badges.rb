471613ebeb951e8d7c8356f8e0c5e2e322396b85
Monica Marziano <mmarziano1@gmail.com>
2018-03-31 01:17:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(name)
+  name_array = []
+    name.each do |names|
+      badge = "Hello, my name is #{names}."
+      name_array << badge 
+    end
+  return name_array
+end
+
+def assign_rooms(name)
+  name.each_with_index do |i|
+    room_number = i + 1 
+    puts "Hello, #{i}! You'll be assigned to room #{room_number}!"
+  end 
+end 
